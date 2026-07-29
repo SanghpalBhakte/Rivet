f454f6329a2341a9c6c28bd9617bb9a43641d900
Sanghpal Bhakte <sanghpal@example.com>
2026-07-29 10:38:51 +0530

    chore: align build version marker hash

--- src/rivet/config/buildInfo.ts
+++ src/rivet/config/buildInfo.ts
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   version: '1.0.4',
   target: 'Cloudflare Pages',
-  commitHash: '38752b3',
+  commitHash: '28a0d18',
   buildTag: 'cf-pages-prod',
   buildDate: '2026-07-29',
 };
